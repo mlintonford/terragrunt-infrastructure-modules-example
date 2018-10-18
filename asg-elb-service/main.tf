d6b1f72070d7e3decd17e1e9e104216177e3e102
mlintonford <matthew@mlfvs.info>
2018-10-18 16:38:00 +0100

    Modified AZs pursuant to us-east-1f not supporting CLBs x2

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -23,7 +23,7 @@ terraform {
 
 resource "aws_autoscaling_group" "webserver_example" {
   launch_configuration = "${aws_launch_configuration.webserver_example.id}"
-  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  availability_zones   = ["us-east-1a"]
 
   load_balancers    = ["${aws_elb.webserver_example.name}"]
   health_check_type = "ELB"
