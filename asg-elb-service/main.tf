1e847112769e7628aeb96a3fbe0fb66384112229
mlintonford <matthew@mlfvs.info>
2018-10-18 17:13:44 +0100

    Hard-coded aws_security_group_rule.elb_allow_all_outbound security_group_id to check if it works at all

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -165,5 +165,5 @@ resource "aws_security_group_rule" "elb_allow_all_outbound" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.elb.id}"
+  security_group_id = "sg-0bed8a8934c987141"
 }
