97b8ea27ff423d3cd0ed11420018ad1369069139
mlintonford <matthew@mlfvs.info>
2018-10-19 12:53:15 +0100

    reverted all aws_security_group.elb.id and added subnets to aws_elb

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -121,8 +121,8 @@ resource "aws_security_group_rule" "asg_allow_http_inbound" {
 resource "aws_elb" "webserver_example" {
   name               = "${var.name}"
   availability_zones = ["${data.aws_availability_zone.all.name}"]
-  security_groups    = ["sg-d60c13ba"]
-
+  security_groups    = ["${aws_security_group.elb.id}"]
+  subnets            = ["subnet-67b9520f"]
 
   listener {
     lb_port           = "${var.elb_port}"
@@ -157,7 +157,7 @@ resource "aws_security_group_rule" "elb_allow_http_inbound" {
   to_port           = "${var.elb_port}"
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "sg-d60c13ba"
+  security_group_id = "${aws_security_group.elb.id}"
 }
 
 resource "aws_security_group_rule" "elb_allow_all_outbound" {
@@ -166,5 +166,5 @@ resource "aws_security_group_rule" "elb_allow_all_outbound" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "sg-d60c13ba"
+  security_group_id = "${aws_security_group.elb.id}"
 }
