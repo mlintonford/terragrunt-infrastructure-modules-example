59749f42f91b68ca4e929f7388963896eae8f30e
mlintonford <matthew@mlfvs.info>
2018-10-22 12:11:09 +0100

    removed aws_autoscaling_group availability_zones to circumvent error

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -23,7 +23,6 @@ terraform {
 
 resource "aws_autoscaling_group" "webserver_example" {
   launch_configuration = "${aws_launch_configuration.webserver_example.id}"
-  availability_zones   = ["${data.aws_availability_zone.all.name}"]
 
   load_balancers    = ["${aws_elb.webserver_example.name}"]
   health_check_type = "ELB"
