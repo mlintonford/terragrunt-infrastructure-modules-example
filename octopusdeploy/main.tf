e23782c6c5558554e1cd3b7e81df7f854adbce48
mlintonford <matthew@mlfvs.info>
2019-02-20 03:42:06 +0000

    Added installation script to user_data

--- octopusdeploy/main.tf
+++ octopusdeploy/main.tf
@@ -54,11 +54,56 @@ resource "aws_launch_configuration" "webserver_example" {
   instance_type   = "${var.instance_type}"
   security_groups = ["${aws_security_group.asg.id}"]
 
-  //user_data = <<-EOF
-  //            #!/bin/bash
-  //            echo "Hello, World" > index.html
-  //            nohup busybox httpd -f -p "${var.server_port}" &
-  //            EOF
+  user_data = <<-EOF
+              Install-Module -Name OctopusDSC -RequiredVersion 4.0.250
+              
+              Configuration SampleConfig
+              {
+                  Import-DscResource -Module OctopusDSC
+
+                  Node "localhost"
+                  {
+                      cOctopusServer OctopusServer
+                      {
+                          Ensure = "Present"
+                          State = "Started"
+
+                          # Server instance name. Leave it as 'OctopusServer' unless you have more than one instance
+                          Name = "OctopusServer"
+
+                          # The url that Octopus will listen on
+                          WebListenPrefix = "http://localhost:80"
+
+                          SqlDbConnectionString = "Server=(local)\SQLEXPRESS;Database=Octopus;Trusted_Connection=True;"
+
+                          # The admin user to create
+                          OctopusAdminUsername = "admin"
+                          OctopusAdminPassword = "<my secret password>"
+
+                          # optional parameters
+                          AllowUpgradeCheck = $true
+                          AllowCollectionOfAnonymousUsageStatistics = $true
+                          ForceSSL = $false
+                          ListenPort = 10943
+                          DownloadUrl = "https://octopus.com/downloads/latest/WindowsX64/OctopusServer"
+                      }
+                  }
+              }
+
+              # Execute the configuration above to create a mof file
+              SampleConfig
+
+              # Run the configuration
+              Start-DscConfiguration -Path ".\SampleConfig" -Verbose -wait
+
+              # Test the configuration ran successfully
+              Test-DscConfiguration
+
+
+              //#!/bin/bash
+              //echo "Hello, World" > index.html
+              //nohup busybox httpd -f -p "${var.server_port}" &
+              //EOF
 
   lifecycle {
     create_before_destroy = true
